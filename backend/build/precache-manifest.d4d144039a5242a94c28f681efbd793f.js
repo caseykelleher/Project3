7e577c0d8d9e7bcc1ea761c92603c3e3f8173895
Casey K <caseykelleher@icloud.com>
2020-03-23 17:52:37 -0400

    frontend

---
+++ backend/build/precache-manifest.d4d144039a5242a94c28f681efbd793f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9861ba6332f4dbbd2ffa5d4525709f05",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76dab3a952b7aa39eb4d",
+    "url": "./static/css/2.6cce2435.chunk.css"
+  },
+  {
+    "revision": "41704ced66febf66e22d",
+    "url": "./static/css/main.d55ca3c3.chunk.css"
+  },
+  {
+    "revision": "76dab3a952b7aa39eb4d",
+    "url": "./static/js/2.cd32353b.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.cd32353b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41704ced66febf66e22d",
+    "url": "./static/js/main.883cce52.chunk.js"
+  },
+  {
+    "revision": "a697cbf8b3fbec7942b2",
+    "url": "./static/js/runtime-main.5a4027cc.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "4ec42e26283c7d88eb53b5340a9b1e24",
+    "url": "./static/media/raining-in-the-city-2448749.4ec42e26.jpg"
+  }
+]);
--- jazz/src/App.js
+++ jazz/src/App.js
@@ -1,12 +1,80 @@
-import React from "react";
+import React        from "react";
+import { observer } from 'mobx-react';
+import UserStore    from './Stores/UserStore';
+import LoginForm    from './LoginForm';
+import SubmitButton from './components/SubmitButton';
+import                   './App.css';
+import Titles       from "./components/Titles";
+import Form         from "./components/Form";
+import Weather      from "./components/Weather";
 
-import Titles from "./components/Titles";
-import Form from "./components/Form";
-import Weather from "./components/Weather";
+
+
 
 const API_KEY = "938c0b043e3bf588186831606cc2990f";
 
 class App extends React.Component {
+
+    async componentDidMount() {
+
+        try {
+
+            var res = await fetch('/isLoggedIn', {
+                method: 'post',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                }
+            });
+
+            var result = await res.json();
+
+            if (result && result.success) {
+              UserStore.loading = false;
+              UserStore.isLoggedIn = true;
+              UserStore.username = result.username;
+            }
+
+            else {
+              UserStore.loading = false;
+              UserStore.isLoggedIn = false;
+
+            }
+
+        }
+
+        catch(e) {
+          UserStore.loading = false;
+          UserStore.isLoggedIn = false; 
+        }
+    }
+
+    async doLogout() {
+
+      try {
+
+          var res = await fetch('/logout', {
+              method: 'post',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
+          });
+
+          var result = await res.json();
+
+          if (result && result.success) {
+            UserStore.isLoggedIn = false;
+            UserStore.username = '';
+          }
+
+      }
+
+      catch(e) {
+        console.log(e)
+      }
+  }
+
   state = {
     temperature: undefined,
     city: undefined,
@@ -42,8 +110,42 @@ class App extends React.Component {
     }
   }
   render() {
+
+      if (UserStore.loading) {
+        return(
+          <div className="app">
+              <div className='container'>
+                  Loading, please wait..
+              </div>
+          </div>
+        );
+      }
+
+      else {
+
+        if (UserStore.isLoggedIn) {
+          return(
+            <div className="app">
+                <div className='container'>
+                    Welcome {UserStore.username}
+
+                    <SubmitButton
+                      text={'Log out'}
+                      disabled={false}
+                      onClick={ () => this.doLogout() }
+                    />
+                </div>
+            </div>
+          );
+        }
+
+      }
+
+
     return (
       <div>
+        <LoginForm />
+
         <div className="wrapper">
           <div className="main">
             <div className="col-xs-5 title-container">
@@ -67,4 +169,4 @@ class App extends React.Component {
   }
 };
 
-export default App;
+export default observer(App);
